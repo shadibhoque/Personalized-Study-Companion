3dae4abc71648a937c290ea0ececbdfcd039b26a
Marie-Lenora <90164392+Marie-Lenora@users.noreply.github.com>
2025-03-15 00:48:27 -0500

    Added todo files

---
+++ todo_script.js
@@ -0,0 +1,46 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something")
+    }
+
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        let img = document.createElement("img");
+        img.src = "images/delete.png";
+        img.alt = "Delete"
+        img.className = "delete-icon"
+        li.appendChild(span)
+        li.appendChild(img)
+
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName == "IMG") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
